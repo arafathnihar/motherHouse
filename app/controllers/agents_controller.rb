b529fc907029d313709893e9213099b32fed2bd5
rizanzaky <rizan149@gmail.com>
2016-06-18 13:01:20 +0530

    agents

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,9 +1,9 @@
-class MotherhouseController < ApplicationController
-  before_action :get_motherhouse, except: [:index, :create]
+class AgentsController < ApplicationController
+  before_action :get_agent, except: [:index, :create]
   respond_to :html, :json
 
   def index
-    @thispara = MainAgent.all
+    @thispara = Agent.all
     respond_with(@thispara) do |format|
       format.json { render :json => @thispara.as_json }
       format.html
@@ -11,7 +11,7 @@ class MotherhouseController < ApplicationController
   end
 
   def create
-    @thispara = MainAgent.new(this_params)
+    @thispara = Agent.new(this_params)
 
     @thispara.guid = SecureRandom.uuid
     @thispara.created_by = 1
@@ -39,19 +39,19 @@ class MotherhouseController < ApplicationController
   end
 
   def destroy
-    @thispara = MainAgent.find(params[:id])
+    @thispara = Agent.find(params[:id])
     @thispara.destroy
     render json: {status: :ok}
   end
 
   private
 
-  def get_motherhouse
-    @thispara = MainAgent.find(params[:id])
+  def get_agent
+    @thispara = Agent.find(params[:id])
     render json: {status: :not_found} unless @thispara
   end
 
   def this_params
-    params.fetch(:requestdata, {}).permit(:customId, :name, :contact, :countryId)
+    params.fetch(:requestdata, {}).permit(:customId, :mainAgentId, :name, :contact, :countryId)
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
-class MotherhouseController < ApplicationController
-  before_action :get_motherhouse, except: [:index, :create]
+class OrdersController < ApplicationController
+  before_action :get_orders, except: [:index, :create]
   respond_to :html, :json
 
   def index
-    @thispara = MainAgent.all
+    @thispara = Order.all
     respond_with(@thispara) do |format|
       format.json { render :json => @thispara.as_json }
       format.html
@@ -11,7 +11,7 @@ class MotherhouseController < ApplicationController
   end
 
   def create
-    @thispara = MainAgent.new(this_params)
+    @thispara = Order.new(this_params)
 
     @thispara.guid = SecureRandom.uuid
     @thispara.created_by = 1
@@ -39,19 +39,20 @@ class MotherhouseController < ApplicationController
   end
 
   def destroy
-    @thispara = MainAgent.find(params[:id])
+    @thispara = Order.find(params[:id])
     @thispara.destroy
     render json: {status: :ok}
   end
 
   private
 
-  def get_motherhouse
-    @thispara = MainAgent.find(params[:id])
+  def get_orders
+    @thispara = Order.find(params[:id])
     render json: {status: :not_found} unless @thispara
   end
 
   def this_params
-    params.fetch(:requestdata, {}).permit(:customId, :name, :contact, :countryId)
+    params.fetch(:requestdata, {})
+        .permit(:customId, :agentId, :receivingAgentId, :receiverId, :orderAmount, :orderCurrId, :supplyCurrId, :exchangeRate, :date, :orderStatus)
   end
 end
