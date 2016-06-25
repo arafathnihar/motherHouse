0d4f74d3fdd21cfa3c7dd805d1566d5ff19170c7
arafath nihar <arafathnihar@gmail.com>
2016-06-25 16:43:50 +0530

    add order form progress

--- app/models/receiving_agent.rb
+++ app/models/receiving_agent.rb
@@ -1,6 +1,6 @@
 class ReceivingAgent < ActiveRecord::Base
 
-  belongs_to :country
+  #belongs_to :country
 
   validates :mainAgentId, presence: { message: "main agent id is required" }
   validates :name, presence: { message: "name is required" }
--- public/client/app.js
+++ public/client/app.js
@@ -1,6 +1,11 @@
 var myApp = angular.module('motherHouse', ['ui.bootstrap', 'ngTable', 'ui.router']);
 
-myApp.controller('AddAgentCtrl', function(WebService, $state) {
+var agentList;
+var receivingAgentList;
+var countries;
+var currencies;
+
+myApp.controller('AddAgentCtrl', function(WebService, UtilityService, $state) {
     var vm = this;
     vm.agent = {
         customId: '',
@@ -9,15 +14,23 @@ myApp.controller('AddAgentCtrl', function(WebService, $state) {
         countryId: '',
         "mainAgentId": "1"
     };
-
+    if (!countries) {
+        countryReqest = UtilityService.getCountries();
+        countryReqest.then(function(data) {
+            countries = data.data;
+            vm.countries = countries;
+        });
+    } else {
+        vm.countries = countries;
+    }
     if ($state.params.agetId) {
         vm.title = "Edit Agent"
-        var reqestObject = {
+        var agentReqestObject = {
             method: 'GET',
             url: '/api/v1/agents/' + $state.params.agetId
         }
-        var reqest = WebService.callWebService(reqestObject);
-        reqest.then(function(data) {
+        var agentReqest = WebService.callWebService(agentReqestObject);
+        agentReqest.then(function(data) {
             var agent = data.data;
             vm.agent.id = agent.id;
             vm.agent.customId = agent.customId;
@@ -26,7 +39,7 @@ myApp.controller('AddAgentCtrl', function(WebService, $state) {
             vm.agent.countryId = agent.countryId;
         });
         vm.addAgent = function() {
-            var reqestObject = {
+            var updateAgentReqestObject = {
                 method: 'PUT',
                 url: '/api/v1/agents/' + vm.agent.id,
                 headers: {
@@ -34,22 +47,92 @@ myApp.controller('AddAgentCtrl', function(WebService, $state) {
                 },
                 data: { requestdata: vm.agent }
             }
-            var reqest = WebService.callWebService(reqestObject);
+            var updateAgentReqest = WebService.callWebService(updateAgentReqestObject);
 
-            reqest.then(function(data) {
+            updateAgentReqest.then(function(data) {
                 console.log(data)
             })
         }
     } else {
         vm.title = "Add New Agent"
         vm.addAgent = function() {
-            var reqestObject = {
+            var addAgentReqestObject = {
                 method: 'POST',
                 url: '/api/v1/agents',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 data: { requestdata: vm.agent }
+            }
+            var addAgentReqest = WebService.callWebService(addAgentReqestObject);
+
+            addAgentReqest.then(function(data) {
+                console.log(data)
+            })
+        }
+    }
+
+});
+myApp.controller('AddReceivingAgentCtrl', function(UtilityService, WebService, $state) {
+    var vm = this;
+
+    if (!countries) {
+        countryReqest = UtilityService.getCountries();
+        countryReqest.then(function(data) {
+            countries = data;
+            vm.countries = countries;
+        });
+    } else {
+        vm.countries = countries;
+    }
+    vm.receivingagent = {
+        customId: '',
+        name: '',
+        contact: '',
+        countryId: '',
+        "mainAgentId": "1"
+    };
+
+    if ($state.params.receivingagentId) {
+        vm.title = "Edit Receiving Agent"
+        var reqestObject = {
+            method: 'GET',
+            url: '/api/v1/receiving_agents/' + $state.params.receivingagentId
+        }
+        var reqest = WebService.callWebService(reqestObject);
+        reqest.then(function(data) {
+            var receivingagent = data.data;
+            vm.receivingagent.id = receivingagent.id;
+            vm.receivingagent.customId = receivingagent.customId;
+            vm.receivingagent.name = receivingagent.name;
+            vm.receivingagent.contact = receivingagent.contact;
+            vm.receivingagent.countryId = receivingagent.countryId;
+        });
+        vm.addReceivingAgent = function() {
+            var reqestObject = {
+                method: 'PUT',
+                url: '/api/v1/receiving_agents/' + vm.receivingagent.id,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: { requestdata: vm.receivingagent }
+            }
+            var reqest = WebService.callWebService(reqestObject);
+
+            reqest.then(function(data) {
+                console.log(data)
+            });
+        }
+    } else {
+        vm.title = "Add New Receiving Agent"
+        vm.addReceivingAgent = function() {
+            var reqestObject = {
+                method: 'POST',
+                url: '/api/v1/receiving_agents',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: { requestdata: vm.receivingagent }
             }
             var reqest = WebService.callWebService(reqestObject);
 
@@ -58,10 +141,8 @@ myApp.controller('AddAgentCtrl', function(WebService, $state) {
             })
         }
     }
-
 });
-
-myApp.controller('AddOrderCtrl', function() {
+myApp.controller('AddOrderCtrl', function(UtilityService) {
     var vm = this;
     vm.order = {
         agentId: '',
@@ -69,17 +150,15 @@ myApp.controller('AddOrderCtrl', function() {
         orderAmount: 0,
         orderCurrId: '',
         supplyCurrId: '',
-        exchangeRate: '',
+        exchangeRate: 0,
         orderStatus: '',
         name: '',
         contact: '',
         countryId: '',
         bank: '',
         branch: '',
-        accountNumber: '',
-        unitPrice: 0
+        accountNumber: ''
     };
-    
     var datepickerConf = function(obj) {
         obj.today = function() {
             obj.date = new Date();
@@ -103,7 +182,6 @@ myApp.controller('AddOrderCtrl', function() {
         obj.datePopup = {
             opened: false
         };
-
     };
     vm.order = {};
     datepickerConf(vm.order);
@@ -111,19 +189,59 @@ myApp.controller('AddOrderCtrl', function() {
     datepickerConf(vm.settled);
     vm.order.today();
     vm.settled.today();
+    if (agentList) {
+        vm.agents = agentList;
+    } else {
+        var agents = UtilityService.getAgentList();
+        agents.then(function(data) {
+            vm.agents = data;
+        });
+    }
+    if (receivingAgentList) {
+        vm.receivingAgents = receivingAgentList;
+    } else {
+        var receivingAgents = UtilityService.getReceivingAgentList();
+        receivingAgents.then(function(data) {
+            vm.receivingAgents = data;
+        });
+    }
+    if (!currencies) {
+        countryReqest = UtilityService.getCurrencies();
+        countryReqest.then(function(data) {
+            currencies = data;
+            vm.currencies = currencies;
+        });
+    } else {
+        vm.currencies = currencies;
+    }
+    if (!countries) {
+        countryReqest = UtilityService.getCountries();
+        countryReqest.then(function(data) {
+            countries = data;
+            vm.countries = countries;
+        });
 
-
+    } else {
+        vm.countries = countries;
+    }
 });
-myApp.controller('agentListCtrl', function(NgTableParams, WebService, $state) {
+
+myApp.controller('agentListCtrl', function(UtilityService, NgTableParams, WebService, $state) {
     var vm = this;
+    if (!countries) {
+        countryReqest = UtilityService.getCountries();
+        countryReqest.then(function(data) {
+            countries = data;
+            vm.countries = countries;
+        });
+    } else {
+        vm.countries = countries;
+    }
     var loadAgents = function() {
-        var reqestObject = {
-            method: 'GET',
-            url: '/api/v1/agents'
-        }
-        var reqest = WebService.callWebService(reqestObject);
-        reqest.then(function(responseData) {
-            vm.data = responseData.data;
+        var agents = UtilityService.getAgentList();
+        agents.then(function(data) {
+            agentList = data;
+            vm.data = agentList;
             vm.tableParams = new NgTableParams({ count: vm.data.length }, { counts: [], data: vm.data });
         });
     }
@@ -148,7 +266,37 @@ myApp.controller('agentListCtrl', function(NgTableParams, WebService, $state) {
         $state.go('addAgent');
     }
 });
+myApp.controller('receivingAgentListCtrl', function(UtilityService, NgTableParams, WebService, $state) {
+    var vm = this;
+    var loadReceivingAgents = function() {
+        var receivingAgents = UtilityService.getReceivingAgentList();
+        receivingAgents.then(function(data) {
+            receivingAgentList = data;
+            vm.data = receivingAgentList;
+            vm.tableParams = new NgTableParams({ count: vm.data.length }, { counts: [], data: vm.data });
+        });
+    }
 
+    loadReceivingAgents();
+    vm.delete = function(receivingAgentId) {
+        var reqestObject = {
+            method: 'DELETE',
+            url: '/api/v1/receiving_agents/' + receivingAgentId
+        }
+        var reqest = WebService.callWebService(reqestObject);
+        reqest.then(function(data) {
+            loadReceivingAgents();
+        });
+    }
+
+    vm.edit = function(receivingAgentId) {
+        $state.go('addReceivingAgent', { 'receivingAgentId': receivingAgentId });
+    }
+
+    vm.addReceivingAgent = function(agetId) {
+        $state.go('addReceivingAgent');
+    }
+});
 myApp.controller('orderListCtrl', function($state) {
     var vm = this;
     var loadAgents = function() {
@@ -180,6 +328,50 @@ myApp.controller('orderListCtrl', function($state) {
     }
 });
 
+myApp.factory('UtilityService', function(WebService) {
+    return {
+        getCountries: function() {
+            var countryReqestObject = {
+                method: 'GET',
+                url: '/commons/countries/'
+            }
+            var countryReqest = WebService.callWebService(countryReqestObject).then(function(data) {
+                return data.data;
+            });
+            return countryReqest;
+        },
+        getAgentList: function() {
+            var agentListReqestObject = {
+                method: 'GET',
+                url: '/api/v1/agents'
+            }
+            var agentListReqest = WebService.callWebService(agentListReqestObject).then(function(responseData) {
+                return responseData.data
+            });
+            return agentListReqest;
+        },
+        getCurrencies: function() {
+            var currencyReqestObject = {
+                method: 'GET',
+                url: '/commons/currencies/'
+            }
+            var currencyReqest = WebService.callWebService(currencyReqestObject).then(function(data) {
+                return data.data;
+            });
+            return currencyReqest;
+        },
+        getReceivingAgentList: function() {
+            var receivingAgentListReqestObject = {
+                method: 'GET',
+                url: '/api/v1/receiving_agents'
+            }
+            var receivingAgentListReqest = WebService.callWebService(receivingAgentListReqestObject).then(function(data) {
+                return data.data;
+            });
+            return receivingAgentListReqest
+        }
+    }
+});
 myApp.factory('WebService', function($http) {
     return {
         callWebService: function(reqestObject) {
@@ -187,10 +379,8 @@ myApp.factory('WebService', function($http) {
         }
     }
 });
-
-
 myApp.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/addOrder");
+    $urlRouterProvider.otherwise("/addReceivingAgent");
     $stateProvider
         .state('addAgent', {
             url: "/addAgent/:agetId",
@@ -208,5 +398,14 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
         .state('addOrder', {
             url: "/addOrder",
             templateUrl: "/client/templates/addOrder.html"
+        })
+        .state('addReceivingAgent', {
+            url: "/addReceivingAgent/:receivingAgentId",
+            params: { receivingAgentId: undefined },
+            templateUrl: "/client/templates/addReceivingAgent.html"
+        })
+        .state('receivingAgentList', {
+            url: "/receivingAgentList",
+            templateUrl: '/client/templates/receivingAgentList.html'
         });
 });
