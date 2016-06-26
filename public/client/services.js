e0d17227e0c721ab0f1858019c6bd37338bcdf8f
arafath nihar <arafathnihar@gmail.com>
2016-06-26 10:06:16 +0530

    code seperated

---
+++ public/client/services.js
@@ -0,0 +1,53 @@
+var myApp = angular.module('motherHouseServices', []);
+
+myApp.factory('UtilityService', function(WebService) {
+    return {
+        getCountries: function() {
+            var countryReqestObject = {
+                method: 'GET',
+                url: '/commons/countries/'
+            }
+            var countryReqest = WebService.callWebService(countryReqestObject).then(function(data) {
+                return data.data;
+            });
+            return countryReqest;
+        },
+        getAgentList: function() {
+            var agentListReqestObject = {
+                method: 'GET',
+                url: '/api/v1/agents'
+            }
+            var agentListReqest = WebService.callWebService(agentListReqestObject).then(function(responseData) {
+                return responseData.data
+            });
+            return agentListReqest;
+        },
+        getCurrencies: function() {
+            var currencyReqestObject = {
+                method: 'GET',
+                url: '/commons/currencies/'
+            }
+            var currencyReqest = WebService.callWebService(currencyReqestObject).then(function(data) {
+                return data.data;
+            });
+            return currencyReqest;
+        },
+        getReceivingAgentList: function() {
+            var receivingAgentListReqestObject = {
+                method: 'GET',
+                url: '/api/v1/receiving_agents'
+            }
+            var receivingAgentListReqest = WebService.callWebService(receivingAgentListReqestObject).then(function(data) {
+                return data.data;
+            });
+            return receivingAgentListReqest
+        }
+    }
+});
+myApp.factory('WebService', function($http) {
+    return {
+        callWebService: function(reqestObject) {
+            return $http(reqestObject);
+        }
+    }
+});
