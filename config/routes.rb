a1698f5c7fe990f8fef40a7a0e569e468e3e5ad4
rizanzaky <rizan149@gmail.com>
2016-07-17 19:49:15 +0530

    clean the code for release v1.3 (bug fix)

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
 
       get '/agentaccount/:fromDate/:toDate', to: 'agentaccount#index'
       get '/agentaccount/:id/:fromDate/:toDate', to: 'agentaccount#agent_index'
-      get '/agentaccount/:id/:id/:id/:id', to: 'agentaccount#payments'
-      get '/agentaccount/:id', to: 'agentaccount#show'
+      get '/agentaccount/:id', to: 'agentaccount#payments'
+      get '/agentaccount/:id/:id/:id/:id', to: 'agentaccount#show'
       post '/agentaccount', to: 'agentaccount#create'
       put '/agentaccount/:id', to: 'agentaccount#update'
       delete '/agentaccount/:id', to: 'agentaccount#nullify'
